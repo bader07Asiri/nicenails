2d05b949093f1241b6240c5697cf043a6b23bf62
NiceNail <info@nailart.sa>
2026-06-29 11:17:41 +0300

    Remove push log from repo and gitignore it

--- push-to-github.bat
+++ push-to-github.bat
@@ -7,8 +7,9 @@ set "LOG=%~dp0git-push-log.txt"
 git --version >> "%LOG%" 2>&1
 git init >> "%LOG%" 2>&1
 git branch -M main >> "%LOG%" 2>&1
+git rm --cached git-push-log.txt >> "%LOG%" 2>&1
 git add -A >> "%LOG%" 2>&1
-git -c user.email=info@nailart.sa -c user.name=NiceNail commit -m "Add recycle bin (soft-delete+restore), refunds & payment-aware delete guard, finance cleanup, inbox media + WhatsApp import" >> "%LOG%" 2>&1
+git -c user.email=info@nailart.sa -c user.name=NiceNail commit -m "Remove push log from repo and gitignore it" >> "%LOG%" 2>&1
 git remote remove origin >> "%LOG%" 2>&1
 git remote add origin https://github.com/bader07Asiri/nicenails.git >> "%LOG%" 2>&1
 git push -u origin main --force >> "%LOG%" 2>&1
